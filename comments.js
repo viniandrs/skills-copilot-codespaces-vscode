783067b9148f770ef8730218918054d321c0e264
Vinicius Andreossi <71352251+viniandrs@users.noreply.github.com>
2023-12-27 21:00:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -6,25 +6,8 @@ var url = require('url');
 var items = [];
 
 var server = http.createServer(function(req, res) {
-	switch (req.method) {
-		case 'POST':
-			var item = '';
-			req.setEncoding('utf8');
-			req.on('data', function(chunk) {
-				item += chunk;
-			});
-			req.on('end', function() {
-				items.push(item);
-				res.end('OK\n');
-			});
-			break;
-		case 'GET':
-			items.forEach(function(item, i) {
-				res.write(i + ') ' + item + '\n');
-			});
-			res.end();
-			break;
+    console.log('Request received.');
 	}
-});
+);
 
 server.listen(3000); 
